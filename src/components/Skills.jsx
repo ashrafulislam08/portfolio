dc352cc7877a01a3f0c9757168f97fcfc2ec1719
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-03-01 10:42:06 +0600

    Added skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,49 @@
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="px-5">
+      <h2 className="text-4xl text-center font-semibold underline decoration-red-500">
+        Skills
+      </h2>
+      <div className="grid grid-cols-4 max-w-2xl mx-auto mt-5">
+        <img src="https://img.icons8.com/?size=48&id=20909&format=png" alt="" />
+        <img src="https://img.icons8.com/?size=48&id=21278&format=png" alt="" />
+        <img
+          src="https://img.icons8.com/?size=48&id=108784&format=png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/?size=48&id=CIAZz2CYc6Kc&format=png"
+          alt=""
+        />
+        <img src="https://img.icons8.com/?size=48&id=54087&format=png" alt="" />
+        <img
+          src="https://img.icons8.com/?size=48&id=PZQVBAxaueDJ&format=png"
+          alt=""
+        />
+        <img src="https://img.icons8.com/?size=48&id=74402&format=png" alt="" />
+        <img src="https://img.icons8.com/?size=48&id=62452&format=png" alt="" />
+        <img src="https://img.icons8.com/?size=48&id=20906&format=png" alt="" />
+        <img
+          src="https://img.icons8.com/?size=48&id=AZOZNnY73haj&format=png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/?size=80&id=sBo1RJ3rjbje&format=png"
+          alt=""
+        />
+        <img
+          className="w-20"
+          src="https://www.svgrepo.com/show/354512/vercel.svg"
+          alt=""
+        />
+        <img
+          className="w-20"
+          src="https://img.icons8.com/?size=100&id=48455&format=png&color=000000"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
