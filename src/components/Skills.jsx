d65cd0a14b09afcd1ae0ef3675ed839e9759c7db
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-03-01 10:55:07 +0600

    Updated skils section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,30 +4,59 @@ const Skills = () => {
       <h2 className="text-4xl text-center font-semibold underline decoration-red-500">
         Skills
       </h2>
-      <div className="grid grid-cols-4 max-w-2xl mx-auto mt-5">
-        <img src="https://img.icons8.com/?size=48&id=20909&format=png" alt="" />
-        <img src="https://img.icons8.com/?size=48&id=21278&format=png" alt="" />
+      <div className="flex flex-wrap gap-3 items-center justify-center max-w-2xl mx-auto mt-5">
         <img
+          className="w-24"
+          src="https://img.icons8.com/?size=48&id=20909&format=png"
+          alt=""
+        />
+        <img
+          className="w-24"
+          src="https://img.icons8.com/?size=48&id=21278&format=png"
+          alt=""
+        />
+        <img
+          className="w-24"
           src="https://img.icons8.com/?size=48&id=108784&format=png"
           alt=""
         />
         <img
+          className="w-24"
           src="https://img.icons8.com/?size=48&id=CIAZz2CYc6Kc&format=png"
           alt=""
         />
-        <img src="https://img.icons8.com/?size=48&id=54087&format=png" alt="" />
         <img
+          className="w-24"
+          src="https://img.icons8.com/?size=48&id=54087&format=png"
+          alt=""
+        />
+        <img
+          className="w-24"
           src="https://img.icons8.com/?size=48&id=PZQVBAxaueDJ&format=png"
           alt=""
         />
-        <img src="https://img.icons8.com/?size=48&id=74402&format=png" alt="" />
-        <img src="https://img.icons8.com/?size=48&id=62452&format=png" alt="" />
-        <img src="https://img.icons8.com/?size=48&id=20906&format=png" alt="" />
         <img
+          className="w-24"
+          src="https://img.icons8.com/?size=48&id=74402&format=png"
+          alt=""
+        />
+        <img
+          className="w-24"
+          src="https://img.icons8.com/?size=48&id=62452&format=png"
+          alt=""
+        />
+        <img
+          className="w-24"
+          src="https://img.icons8.com/?size=48&id=20906&format=png"
+          alt=""
+        />
+        <img
+          className="w-24"
           src="https://img.icons8.com/?size=48&id=AZOZNnY73haj&format=png"
           alt=""
         />
         <img
+          className="w-24"
           src="https://img.icons8.com/?size=80&id=sBo1RJ3rjbje&format=png"
           alt=""
         />
